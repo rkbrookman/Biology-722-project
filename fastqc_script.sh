2e9ee985a52be14888d25f5a29e1c70ae744d385
rkbrookman <123647801+rkbrookman@users.noreply.github.com>
2024-04-24 21:43:22 -0400

    Create fastqc_script.sh

---
+++ fastqc_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#This script runs fastqc on all files in the directory ending in .fastq.gz
+
+for i in *.fastq.gz
+
+do
+
+/usr/local-centos6/fastqc/fastqc "$i"
+
+done
+
+#The following variation was used to run fastqc after trimming the reads:
+
+for i in *.trimmed.fastq.gz
+
+do
+
+/usrt/local-centos6/fastqc/fastqc "$i"
+
+done
