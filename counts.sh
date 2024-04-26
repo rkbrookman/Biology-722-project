beadbb485d591ae360899f760e563f0fef50bf92
rkbrookman <123647801+rkbrookman@users.noreply.github.com>
2024-04-26 06:26:02 -0400

    Updated annotations on counts.sh

--- counts.sh
+++ counts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script counts aligned reads 
+# This script counts aligned reads and throws out reads that mapped ambiguously (ie. to more than one gene)
 
 for i in *.bam
 
@@ -10,7 +10,7 @@ do
 
 done
 
-# The '--format bam' tells htseq we're using a BAM file as input
-# The '--order pos' tells htseq our BAM file is sorted by coordinate
-# '--stranded reverse' indicates the strandedness of our experiment. Since Mine et al. (2018) prepared their library using the dutp method, the library is reversed. I confirmed this with the 'infer_experiment.py' script from RSeQC.
-# The '--type=gene' tag is needed since I used a gff3 file instead of a gtf file. The defualt for this parameter is 'exon', which works for gtf files but didn't work for me. After playing around with different tags, I found that type=gene gave me the output I wanted (counts mapped to genes)
+# The '--format bam' tells htseq-count we're using a BAM file as input
+# The '--order pos' tells htseq-count our BAM file is sorted by coordinate
+# '--stranded reverse' indicates the strandedness of our experiment. Since Mine et al. (2018) prepared their library using a kit that uses the dUTP method, the strandedness should be 'reversed'. I also confirmed this with the 'infer_experiment.py' script from RSeQC.
+# The '--type=gene' tag is needed since I used a gff3 file instead of a gtf file. The defualt for this parameter is 'exon', which works for gtf files but didn't work for me. After playing around with different tags, I found that type=gene seemed to work.
