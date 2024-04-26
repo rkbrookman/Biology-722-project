f8d76c377937b4a1b472d727c9049cb12ac8005a
rkbrookman <123647801+rkbrookman@users.noreply.github.com>
2024-04-25 21:43:35 -0400

    Updated comment on SRA_download.sh

--- SRA_download.sh
+++ SRA_download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#This script reads through the SRA_numbers.txt file, which contains all of the SRA accession numbers required for the project, and then downloads them with prefetch
+#This script reads through the SRA_numbers.txt file (which is just a text file where I wrote all of the SRA accession numbers required for the project) and then downloads them with prefetch
 # Fastq-dump conversts them to fastq format, and the --gzip tag zips them
 # In order for this script to work, miniconda and SRA toolkit are required
 
